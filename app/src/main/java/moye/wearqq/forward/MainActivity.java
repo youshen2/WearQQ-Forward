1b4c9f8c7b9728f575fcc7e87f8d4099b5c8b256
youshen2 <huicat28@163.com>
2024-10-11 19:59:21 +0800

    First push

---
+++ app/src/main/java/moye/wearqq/forward/MainActivity.java
@@ -0,0 +1,13 @@
+package moye.wearqq.forward;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
